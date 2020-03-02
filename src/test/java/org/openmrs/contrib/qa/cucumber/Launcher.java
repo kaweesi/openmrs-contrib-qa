417efc576f1c36a5eae90f16c6a23bf66ec3c6bf
k-joseph <kaweesi@jembi.org>
2020-03-02 23:13:51 +0300

    cleaned launcher

--- src/test/java/org/openmrs/contrib/qa/cucumber/Launcher.java
+++ src/test/java/org/openmrs/contrib/qa/cucumber/Launcher.java
@@ -1,10 +1,10 @@
 package org.openmrs.contrib.qa.cucumber;
 
 import cucumber.api.CucumberOptions;
-import cucumber.api.SnippetType;
 import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin = {"html:target/cucumber-html-report"}, snippets = SnippetType.CAMELCASE,tags = {"@single"}, features = "src/test/resources/features")
+@CucumberOptions(features = "src/test/resources/features",
+        glue = "org.openmrs.contrib.qa.cucumber.StepDefinitions")
 public class Launcher {}
